594a2ffd08aa8a1dc75fd94e72fd546b387e44cf
mufarodev <mufaro.priv@gmail.com>
2026-08-10 10:54:05 +0200

    fix: defer Signal desktop downgrade

--- modules/home/desktop-apps.nix
+++ modules/home/desktop-apps.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
+  # ponytail: Signal stays vendor-managed until nixpkgs supports database schema 1760.
   home.packages = with pkgs; [
     prismlauncher
-    signal-desktop
     zed-editor
   ];
 }
